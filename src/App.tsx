6a04af7c56c50dbc729f6d9bc7abde8d0515df23
lorenzobruni-dev <109805268+lorenzobruni-dev@users.noreply.github.com>
2025-03-06 13:02:31 +0100

    :construction: Ui of content body

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Topbar } from "./pages/topbar/Topbar.tsx";
 import { Footer } from "./pages/footer/Footer.tsx";
+import { BodyApplication } from "./pages/body/BodyApplication.tsx";
 
 const App = () => {
   const isResizedScreen = useMediaQuery("(min-width: 700px)");
   return (
     <Box className={"grid-container"}>
       <Topbar />
-      <Box className={"body-container"}></Box>
+      <BodyApplication />
       {isResizedScreen && <Footer />}
     </Box>
   );
